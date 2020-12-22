afcf1897a32a926aaa64c77d70a29df1b4566a7f
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-12-22 12:03:04 +0100

    Fix parsing of more complex face defs

--- lib/3d/utils/model_parse.lua
+++ lib/3d/utils/model_parse.lua
@@ -11,6 +11,10 @@ local model_parse = {}
 -- ------------------------------------------------------------------------
 -- .OBJ
 
+function normalize_face_vertex_id(vertex_id)
+  return tonumber(split(vertex_id, "/")[1])
+end
+
 function model_parse.obj (file_path)
   local vertices = {}
   local faces = {}
@@ -25,7 +29,10 @@ function model_parse.obj (file_path)
                                 tonumber(cells[3]),
                                 tonumber(cells[4])})
       elseif instruction == "f" then
-        table.insert(faces,  {tonumber(cells[2]), tonumber(cells[3]), tonumber(cells[4])})
+        table.insert(faces,
+                     {normalize_face_vertex_id(cells[2]),
+                      normalize_face_vertex_id(cells[3]),
+                      normalize_face_vertex_id(cells[4])})
       end
     end
   end
